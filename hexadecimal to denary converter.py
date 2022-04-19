fcc9d23a3057bbf82ce4ca2fb8200a82cc9a56e4
Perfect-Gaming <95082028+Perfect-Gaming@users.noreply.github.com>
2022-04-19 20:57:39 +0800

    Add files via upload

---
+++ hexadecimal to denary converter.py
@@ -0,0 +1,14 @@
+import time
+
+print("Enter Hexadecimal Number: ")
+hexadecimal = input()
+hexadecimal = str(hexadecimal)
+converter = int(hexadecimal, 16)
+print(converter)
+    
+
+
+
+
+time.sleep(5)
+quit()
